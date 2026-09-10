7d751ff04a386d52c1a46b9d26802f4fa6a28b21
Dillon Rinkwest <ST10439147@myemeris.edu.za>
2026-09-10 13:50:42 +0200

    Created the morse binary node

---
+++ MorseCode_BinaryTrees/Classes/MorseNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MorseCode_BinaryTrees.Classes
+{
+    public class MorseNode
+    {
+        public char Character { get; set; }
+        public MorseTree Left { get; set; }
+
+        public MorseTree Right { get; set; }
+
+        public MorseNode(char character = ' ')
+        {
+            Character = character;
+            Left = null;
+            Right = null;
+        }
+
+    }
+}
--- MorseCode_BinaryTrees/Classes/MorseTree.cs
+++ MorseCode_BinaryTrees/Classes/MorseTree.cs
@@ -4,7 +4,9 @@ using System.Text;
 
 namespace MorseCode_BinaryTrees.Classes
 {
-    internal class BinaryTree
+    public class MorseTree
     {
+ 
+
     }
 }
